1c20e961e3c69536122a1880695492827d6e878d
mateshwari verma <46149432+mpv33@users.noreply.github.com>
2019-11-15 16:04:21 +0530

    Update settings.py

--- django_bokeh/django_bokeh/settings.py
+++ django_bokeh/django_bokeh/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'neuraltech.urls'
+ROOT_URLCONF = 'django_bokeh.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'neuraltech.wsgi.application'
+WSGI_APPLICATION = 'django_bokeh.wsgi.application'
 
 
 # Database
